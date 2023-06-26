57d255c616055615af4ba7582965066b04485998
Marin <mtpetrov1@gmail.com>
2023-06-26 17:44:07 +0300

    homework-lecture-07 exercise 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter the first number: ");
+            int num1 = scanner.nextInt();
+
+            System.out.print("Enter the second number: ");
+            int num2 = scanner.nextInt();
+
+            double result = num1/num2;
+            System.out.println("Result: " + result);
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input! Please enter numbers only.");
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero is not allowed.");
+        }
     }
+
 }
+
